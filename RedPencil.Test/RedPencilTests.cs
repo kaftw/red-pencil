f6929d1f04fb9622a33ab9f9d932b1d810cc9849
John Kuempel <john.kuempel@gmail.com>
2015-04-11 15:12:45 -0400

    Added first test for 5% rule

---
+++ RedPencil.Test/RedPencilTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RedPencil.Test
+{
+    [TestClass]
+    public class RedPencilTests
+    {
+        [TestMethod]
+        public void RedPencilPromotionTakesEffectAt5PercentDiscount()
+        {
+            Product product = new Product();
+            product.MSRP = 1.0d;
+            product.SalePrice = 0.95d;
+
+            RedPencil promotion = new RedPencil();
+            Assert.AreEqual(true, promotion.IsEligible(product));
+        }
+    }
+}
--- RedPencil/Product.cs
+++ RedPencil/Product.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace RedPencil
 {
-    public class Class1
+    public class Product
     {
+        public double MSRP { get; set; }
+
+        public double SalePrice { get; set; }
     }
 }
