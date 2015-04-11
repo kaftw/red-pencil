d0391567e15797d7255cd2d83a9734fd29151db5
John Kuempel <john.kuempel@gmail.com>
2015-04-11 15:13:27 -0400

    Added RedPencil class

---
+++ RedPencil/RedPencil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedPencil
+{
+    public class RedPencil
+    {
+        public Boolean IsEligible(Product product)
+        {
+            return true;
+        }
+    }
+}
