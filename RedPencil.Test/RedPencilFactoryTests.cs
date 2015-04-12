fb885ffc6fc2d89bf5ab74d4c33192a561bab3cb
John Kuempel <john.kuempel@gmail.com>
2015-04-11 20:14:03 -0400

    Refactor factory tests into factory test class

---
+++ RedPencil.Test/RedPencilFactoryTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RedPencil.Test
+{
+    [TestClass]
+    public class RedPencilFactoryTests
+    {
+        [ExpectedException(typeof(InvalidOperationException))]
+        [TestMethod]
+        public void PromotionCanNotLastMoreThan30Days()
+        {
+            var product = CreateTestProduct();
+            var factory = new RedPencilFactory();
+            var promotion = factory.CreatePromotion(product, DateTime.Now.Subtract(TimeSpan.FromDays(31)), DateTime.Now);
+
+            promotion.Begin(product);
+        }
+
+        [TestMethod]
+        public void PromotionCanLastUpTo30Days()
+        {
+            var product = CreateTestProduct();
+            var factory = new RedPencilFactory();
+            var promotion = factory.CreatePromotion(product, DateTime.Now, DateTime.Now.AddDays(30));
+            promotion.Begin(product);
+        }
+
+        [TestMethod]
+        public void PriceReductionDuringPromotionDoesNotProlongIt()
+        {
+            var product = CreateTestProduct();
+
+            var factory = new RedPencilFactory();
+            var promotion = factory.CreatePromotion(product, DateTime.Now, DateTime.Now.AddDays(30));
+            var promotionLength = promotion.EndDate - promotion.StartDate;
+            promotion.Begin(product);
+            product.SalePrice = .8;
+
+            Assert.AreEqual(promotionLength, promotion.EndDate - promotion.StartDate);
+        }
+
+        [TestMethod]
+        public void PriceIncreaseDuringPromotionEndsIt()
+        {
+            var product = CreateTestProduct();
+
+            var factory = new RedPencilFactory();
+            var promotion = factory.CreatePromotion(product, DateTime.Now, DateTime.Now.AddDays(30));
+            var promotionEnd = promotion.EndDate;
+            promotion.Begin(product);
+            product.SalePrice = .95;
+
+            Assert.AreNotEqual(promotionEnd, promotion.EndDate);
+        }
+
+        [TestMethod]
+        public void PriceReductionToTotalDiscountGreaterThan30PercentEndsPromotion()
+        {
+            var product = CreateTestProduct();
+
+            var factory = new RedPencilFactory();
+            var promotion = factory.CreatePromotion(product, DateTime.Now, DateTime.Now.AddDays(30));
+            var promotionEnd = promotion.EndDate;
+            promotion.Begin(product);
+            product.SalePrice = .6;
+
+            Assert.AreNotEqual(promotionEnd, promotion.EndDate);
+        }
+
+        [TestMethod]
+        public void AnotherPromotionMayBeStartedIfTheyDoNotOverlap()
+        {
+            var product = CreateTestProduct();
+            var factory = new RedPencilFactory();
+            var promotion = factory.CreatePromotion(product, DateTime.Now.Subtract(TimeSpan.FromDays(1)), DateTime.Now);
+            promotion.Begin(product);
+
+            var anotherPromotion = factory.CreatePromotion(product, DateTime.Now.AddMilliseconds(1), DateTime.Now.AddDays(1));
+            anotherPromotion.Begin(product);
+        }
+
+        [ExpectedException(typeof(InvalidOperationException))]
+        [TestMethod]
+        public void AnotherPromotionMayNotBeStartedIfTheyDoOverlap()
+        {
+            var product = CreateTestProduct();
+            var factory = new RedPencilFactory();
+            var promotion = factory.CreatePromotion(product, DateTime.Now.Subtract(TimeSpan.FromDays(1)), DateTime.Now);
+            promotion.Begin(product);
+
+            var anotherPromotion = factory.CreatePromotion(product, DateTime.Now.Subtract(TimeSpan.FromHours(12)), DateTime.Now.AddDays(1));
+            anotherPromotion.Begin(product);
+        }
+
+        private static Product CreateTestProduct()
+        {
+            var product = new Product();
+            product.MSRP = 1;
+            product.SalePrice = .9;
+            product.PreviousPriceChangeOccurredAt = DateTime.Now.Subtract(TimeSpan.FromDays(30));
+            return product;
+        }
+    }
+}
--- RedPencil.Test/RedPencilTests.cs
+++ RedPencil.Test/RedPencilTests.cs
@@ -44,15 +44,6 @@ namespace RedPencil.Test
             Assert.AreEqual(true, RedPencil.IsEligible(product));
         }
 
-        private static Product CreateTestProduct()
-        {
-            var product = new Product();
-            product.MSRP = 1;
-            product.SalePrice = .9;
-            product.PreviousPriceChangeOccurredAt = DateTime.Now.Subtract(TimeSpan.FromDays(30));
-            return product;
-        }
-
         [TestMethod]
         public void PromotionDoesNotTakeEffectWithin30DaysOfPreviousPriceChange()
         {
@@ -60,93 +51,15 @@ namespace RedPencil.Test
             product.PreviousPriceChangeOccurredAt = DateTime.Now;
 
             Assert.AreEqual(false, RedPencil.IsEligible(product));
-        }
+        }        
 
-        [ExpectedException(typeof(InvalidOperationException))]
-        [TestMethod]
-        public void PromotionCanNotLastMoreThan30Days()
+        private static Product CreateTestProduct()
         {
             var product = new Product();
-            var factory = new RedPencilFactory();
-            var promotion = factory.CreatePromotion(product, DateTime.Now.Subtract(TimeSpan.FromDays(31)), DateTime.Now);
-
-            promotion.Begin(product);
-        }
-
-        [TestMethod]
-        public void PromotionCanLastUpTo30Days()
-        {
-            var product = CreateTestProduct();
-            var factory = new RedPencilFactory();
-            var promotion = factory.CreatePromotion(product, DateTime.Now, DateTime.Now.AddDays(30));
-            promotion.Begin(product);
-        }
-
-        [TestMethod]
-        public void PriceReductionDuringPromotionDoesNotProlongIt()
-        {
-            var product = CreateTestProduct();
-
-            var factory = new RedPencilFactory();
-            var promotion = factory.CreatePromotion(product, DateTime.Now, DateTime.Now.AddDays(30));
-            var promotionLength = promotion.EndDate - promotion.StartDate;
-            promotion.Begin(product);
-            product.SalePrice = .8;
-
-            Assert.AreEqual(promotionLength, promotion.EndDate - promotion.StartDate);
-        }
-
-        [TestMethod]
-        public void PriceIncreaseDuringPromotionEndsIt()
-        {
-            var product = CreateTestProduct();
-
-            var factory = new RedPencilFactory();
-            var promotion = factory.CreatePromotion(product, DateTime.Now, DateTime.Now.AddDays(30));
-            var promotionEnd = promotion.EndDate;
-            promotion.Begin(product);
-            product.SalePrice = .95;
-
-            Assert.AreNotEqual(promotionEnd, promotion.EndDate);
-        }
-
-        [TestMethod]
-        public void PriceReductionToTotalDiscountGreaterThan30PercentEndsPromotion()
-        {
-            var product = CreateTestProduct();
-
-            var factory = new RedPencilFactory();
-            var promotion = factory.CreatePromotion(product, DateTime.Now, DateTime.Now.AddDays(30));
-            var promotionEnd = promotion.EndDate;
-            promotion.Begin(product);
-            product.SalePrice = .6;
-
-            Assert.AreNotEqual(promotionEnd, promotion.EndDate);
-        }
-
-        [TestMethod]
-        public void AnotherPromotionMayBeStartedIfTheyDoNotOverlap()
-        {
-            var product = CreateTestProduct();
-            var factory = new RedPencilFactory();
-            var promotion = factory.CreatePromotion(product, DateTime.Now.Subtract(TimeSpan.FromDays(1)), DateTime.Now);
-            promotion.Begin(product);
-
-            var anotherPromotion = factory.CreatePromotion(product, DateTime.Now.AddMilliseconds(1), DateTime.Now.AddDays(1));
-            anotherPromotion.Begin(product); 
-        }
-
-        [ExpectedException(typeof(InvalidOperationException))]
-        [TestMethod]
-        public void AnotherPromotionMayNotBeStartedIfTheyDoOverlap()
-        {
-            var product = CreateTestProduct();
-            var factory = new RedPencilFactory();
-            var promotion = factory.CreatePromotion(product, DateTime.Now.Subtract(TimeSpan.FromDays(1)), DateTime.Now);
-            promotion.Begin(product);
-
-            var anotherPromotion = factory.CreatePromotion(product, DateTime.Now.Subtract(TimeSpan.FromHours(12)), DateTime.Now.AddDays(1));
-            anotherPromotion.Begin(product); 
+            product.MSRP = 1;
+            product.SalePrice = .9;
+            product.PreviousPriceChangeOccurredAt = DateTime.Now.Subtract(TimeSpan.FromDays(30));
+            return product;
         }
     }
 }
